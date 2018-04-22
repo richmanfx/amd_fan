d462c6e04e73a4ebfb4b250b9b0d015d1483a5bf
Aleksandr Jashhuk <richmanfx@yandex.ru>
2018-04-22 18:46:01 +0300

    Create configuration file.

---
+++ amd_fan_config.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+""" Configuration file """
+
+# High temperature, °C
+HIGH_TEMP = 55
+
+# Low temperature, °C
+LOW_TEMP = 40
+
+# Timeout between polls GPU, sec
+SLEEP_TIME = 60
+
+# Speed step, %
+SPEED_STEP = 4
