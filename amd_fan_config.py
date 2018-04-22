069ef795463d5ded1193ec3ac8c9aa39fa6cd37f
Aleksandr Jashhuk <richmanfx@yandex.ru>
2018-04-22 22:14:08 +0300

    Create todo.txt .

--- amd_fan_config.py
+++ amd_fan_config.py
@@ -2,13 +2,13 @@
 """ Configuration file """
 
 # High temperature, °C
-HIGH_TEMP = 50
+HIGH_TEMP = 55
 
 # Low temperature, °C
-LOW_TEMP = 45
+LOW_TEMP = 50
 
 # Timeout between polls GPU, sec
-SLEEP_TIME = 60
+SLEEP_TIME = 300
 
 # Speed step, %
 SPEED_STEP = 5
