7ad6ef8f96c1cfbdc1cc9123a1be3af388421074
jashhuk@vistgroup.ru <jashhuk@vistgroup.ru>
2018-04-23 18:32:53 +0300

    Comments in configuration file.

--- amd_fan_config.py
+++ amd_fan_config.py
@@ -1,17 +1,17 @@
 # -*- coding: utf-8 -*-
 """ Configuration file """
 
-# High temperature, °C
-HIGH_TEMP = 55
+# High temperature, °C (default value - 60)
+HIGH_TEMP = 60
 
-# Low temperature, °C
-LOW_TEMP = 50
+# Low temperature, °C (default value - 55)
+LOW_TEMP = 55
 
-# Timeout between polls GPU, sec
+# Timeout between polls GPU, sec (default value - 300)
 SLEEP_TIME = 300
 
-# Speed step, %
+# Speed step, % (default value - 5)
 SPEED_STEP = 5
 
-# Debug level (DEBUG - debugging)
+# Debug level ('DEBUG' - debugging messages, '' - silence)
 DEBUG_LEVEL = 'DEBUG'
