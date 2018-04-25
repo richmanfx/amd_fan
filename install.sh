d7165b18990145f0743ec68ccc15dccc63e1bdb2
jashhuk@vistgroup.ru <jashhuk@vistgroup.ru>
2018-04-25 12:59:31 +0300

    Файл конфигурации перемещён в директорию с приложением.

--- install.sh
+++ install.sh
@@ -1,36 +1,15 @@
 #!/bin/sh
 
-config_dir=".amd_fan"
 config_file="amd_fan_config.py"
 app_dir="amd_fan"
 app_name="amd_fan.py"
-
-
-# Create directory for configuration file
-if ! [ -d "$HOME/$config_dir" ]
-then
-    echo "The directory '$HOME/$config_dir' not found. Create it."
-    mkdir $HOME/$config_dir
-else
-    echo "The directory '$HOME/$config_dir' already exists".
-fi
-
-
-# To place a configuration file
-if [ -f "$HOME/$config_dir/$config_file" ]
-then
-    echo "Configuration file '$HOME/$config_dir/$config_file' already exists."
-else
-    echo "Copying configuration file '$HOME/$config_dir/$config_file' to '$config_dir' directory"
-    /bin/cp ./$config_file $HOME/$config_dir/$config_file
-fi
 
 
 # Create directory for application
 if ! [ -d "$HOME/bin/$app_dir" ]
 then
     echo "The directory '$HOME/bin/$app_dir' not found. Create it."
-    mkdir $HOME/bin/$app_dir
+    mkdir $HOME/bin/${app_dir}
 else
     echo "The directory '$HOME/bin/$app_dir' already exists".
 fi
@@ -40,12 +19,22 @@ fi
 if [ -f "$HOME/bin/$app_dir/$app_name" ]
 then
     echo "Application file '$HOME/bin/$app_dir/$app_name' already exists. Backup it."
-    mv $HOME/bin/$app_dir/$app_name $HOME/bin/$app_dir/$app_name.backup
+    mv $HOME/bin/${app_dir}/${app_name} $HOME/bin/${app_dir}/${app_name}.backup
     echo "Copying application file '$app_name' to '$HOME/bin/$app_dir/' directory"
-    /bin/cp ./$app_name $HOME/bin/$app_dir/$app_name
+    /bin/cp ./${app_name} $HOME/bin/${app_dir}/${app_name}
 else
     echo "Copying application file '$app_name' to '$HOME/bin/$app_dir/' directory"
-    /bin/cp ./$app_name $HOME/bin/$app_dir/$app_name
+    /bin/cp ./${app_name} $HOME/bin/${app_dir}/${app_name}
+fi
+
+
+# To place a configuration file
+if [ -f "$HOME/bin/$app_dir/$config_file" ]
+then
+    echo "Configuration file '$HOME/bin/$app_dir/$config_file' already exists."
+else
+    echo "Copying configuration file '$config_file' to '$HOME/bin/$app_dir/' directory"
+    /bin/cp ./${config_file} $HOME/bin/${app_dir}/${config_file}
 fi
 
 
